653408ba49a60791d9e49eb2031ae7f10b5f1d78
Salem Kamoun <kammoun.salem@gmail.com>
2026-01-06 06:54:09 +0100

    fiw browser notification

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,7 +4,18 @@
 importScripts('https://www.gstatic.com/firebasejs/10.12.5/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.5/firebase-messaging-compat.js');
 
-firebase.initializeApp({"apiKey":"AIzaSyCuuv5Ct4laED73ejjT88nqxBNDtXubAWI","authDomain":"laprincesse-salledesfetes.firebaseapp.com","projectId":"laprincesse-salledesfetes","storageBucket":"laprincesse-salledesfetes.firebasestorage.app","messagingSenderId":"834193551998","appId":"1:834193551998:web:0e27ae6b42e76ecefc9e2f"});
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCuuv5Ct4laED73ejjT88nqxBNDtXubAWI",
+    authDomain: "laprincesse-salledesfetes.firebaseapp.com",
+    databaseURL: "https://laprincesse-salledesfetes-default-rtdb.firebaseio.com",
+    projectId: "laprincesse-salledesfetes",
+    storageBucket: "laprincesse-salledesfetes.firebasestorage.app",
+    messagingSenderId: "834193551998",
+    appId: "1:834193551998:web:0e27ae6b42e76ecefc9e2f",
+    measurementId: "G-4ER0LC84ER",
+    vapidKey: "BM2RBmBWpexF8AuEX7bJ34DVvtbPi0-9pbP8yYZ7nU8hfR6vSQZvUuZoAF-V96X05k0-ujJLEM55aH9BFLqtNuA"
+});
 
 const messaging = firebase.messaging();
 
--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -3,14 +3,15 @@ importScripts('https://www.gstatic.com/firebasejs/10.9.0/firebase-messaging-comp
 
 // Configuration identique à environment.ts
 firebase.initializeApp({
-  apiKey: "AIzaSyCuuv5Ct4laED73ejjT88nqxBNDtXubAWI",
-  authDomain: "laprincesse-salledesfetes.firebaseapp.com",
-  databaseURL: "https://laprincesse-salledesfetes-default-rtdb.firebaseio.com",
-  projectId: "laprincesse-salledesfetes",
-  storageBucket: "laprincesse-salledesfetes.firebasestorage.app",
-  messagingSenderId: "834193551998",
-  appId: "1:834193551998:web:0e27ae6b42e76ecefc9e2f",
-  measurementId: "G-4ER0LC84ER"
+    apiKey: "AIzaSyCuuv5Ct4laED73ejjT88nqxBNDtXubAWI",
+    authDomain: "laprincesse-salledesfetes.firebaseapp.com",
+    databaseURL: "https://laprincesse-salledesfetes-default-rtdb.firebaseio.com",
+    projectId: "laprincesse-salledesfetes",
+    storageBucket: "laprincesse-salledesfetes.firebasestorage.app",
+    messagingSenderId: "834193551998",
+    appId: "1:834193551998:web:0e27ae6b42e76ecefc9e2f",
+    measurementId: "G-4ER0LC84ER",
+    vapidKey: "BM2RBmBWpexF8AuEX7bJ34DVvtbPi0-9pbP8yYZ7nU8hfR6vSQZvUuZoAF-V96X05k0-ujJLEM55aH9BFLqtNuA"
 });
 
 const messaging = firebase.messaging();
